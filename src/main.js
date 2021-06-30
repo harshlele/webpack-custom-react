80d1aaca9f9a22864b1e6b83091543091eefdb17
Harshal Lele <harshallele97@gmail.com>
2021-06-30 16:00:13 +0530

    added css and babel

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import "./style.css";
+import "./style.scss";
+
 window.onload = function(){
     document.body.innerHTML += "test";
+    console.log(document.body.innerHTML);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,27 @@ module.exports = {
     mode: "development",
     entry: "./src/main.js",
     module: {
-        
+        rules:[ 
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }   
+            },
+            {
+                test: /\.css$/i,
+                use:["style-loader", "css-loader"]
+                
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use:["style-loader", "css-loader", "sass-loader"]
+                
+            }
+        ]
     },
+    devtool: "source-map",
     devServer: {
         contentBase: "./dist"
     }
